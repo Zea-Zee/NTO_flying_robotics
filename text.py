df4a47f3e1f609f82ef37e37ac35d83b69d6f993
Zea-Zee <glebru.irk38@yandex.ru>
2024-03-25 20:55:41 +0700

    ocr

--- text.py
+++ text.py
@@ -3,14 +3,17 @@ from PIL import Image
 import pytesseract
 
 
-img = cv2.imread('./car.jpg')
+img = cv2.imread('./image.png')
+img2 = cv2.imread('./car.jpg')
 
 
 def recognize_text(image):
     image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789АВЕКМНОРСТУХ'
-    text = pytesseract.image_to_string(image_pil, config=custom_config)
+    custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789АВЕКМНОРСТУХavekmhopctyx'
+    text = pytesseract.image_to_string(image_pil, config=custom_config).upper().strip()
+    # text = pytesseract.image_to_string(image_pil)
     return text
 
 
 print(recognize_text(img))
+print(recognize_text(img2))
